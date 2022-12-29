8970324861df83e0b308a86458a9452dabe8c04b
calebmatsick <calebmatsick@gmail.com>
2022-12-29 13:35:04 -0700

    Add improved solutions part 1 & 2 - furthur changes questionable

--- day_2/src/main.rs
+++ day_2/src/main.rs
@@ -1,8 +1,29 @@
 use std::collections::HashMap;
 
-fn main() {
-    let input = include_str!("../input.txt");
+fn original_solution_part1(input: &str) {
+    let rubric = HashMap::from([
+        ("A X", 4),
+        ("A Y", 8),
+        ("A Z", 3),
+        ("B X", 1),
+        ("B Y", 5),
+        ("B Z", 9),
+        ("C X", 7),
+        ("C Y", 2),
+        ("C Z", 6),
+    ]);
 
+    let mut score = 0;
+
+    for lines in input.lines() {
+        let round = rubric[lines];
+        score = score + round;
+    }    
+
+    println!("{}", score);
+}
+
+fn original_solution_part2(input: &str) {
     let rubric = HashMap::from([
         ("A X", 3),
         ("A Y", 4),
@@ -25,9 +46,47 @@ fn main() {
     println!("{}", score);
 }
 
-// A for Rock, B for Paper, and C for Scissors - Opp
-// X for Rock, Y for Paper, and Z - You
-// 1 for Rock, 2 for Paper, and 3 for Scissors
-// 0 if you lost, 3 if the round was a draw, and 6 if you won
+fn improved_solution_part1(input: &str) {
+    let rubric = HashMap::from([
+        ("A X", 4),
+        ("A Y", 8),
+        ("A Z", 3),
+        ("B X", 1),
+        ("B Y", 5),
+        ("B Z", 9),
+        ("C X", 7),
+        ("C Y", 2),
+        ("C Z", 6),
+    ]);
 
-// PART TWO: X - lose, Y - draw, Z - win
+    let score: usize = input.lines().map(|line| rubric[line]).sum();
+
+    println!("{}", score);
+}
+
+fn improved_solution_part2(input: &str) {
+    let rubric = HashMap::from([
+        ("A X", 3),
+        ("A Y", 4),
+        ("A Z", 8),
+        ("B X", 1),
+        ("B Y", 5),
+        ("B Z", 9),
+        ("C X", 2),
+        ("C Y", 6),
+        ("C Z", 7),
+    ]);
+
+    let score: usize = input.lines().map(|line| rubric[line]).sum();
+
+    println!("{}", score);
+}
+
+fn main() {
+    let input = include_str!("../input.txt");
+
+    original_solution_part1(&input);
+    original_solution_part2(&input);
+    improved_solution_part1(&input);
+    improved_solution_part2(&input);
+}
